2d3e04689d8170214c33c6ce8941acc7469ee83e
Passakorn777 <73682396+Passakorn777@users.noreply.github.com>
2020-11-20 19:57:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>All Comments</title>
+    <title>Comments</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
