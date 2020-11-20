b2062a29d44a2c919481f839e7277c87efc49bdf
Passakorn777 <73682396+Passakorn777@users.noreply.github.com>
2020-11-20 20:04:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   <body>
     <?php
     $conn = mysqli_init();
-    mysqli_real_connect($conn, 'bakonawa.mysql.database.azure.com', 'passakorn888@bakonawa', 'Thedarwin9876', 'ITF13', 3306);
+    mysqli_real_connect($conn, 'bakonawa.mysql.database.azure.com', 'passakorn888@bakonawa', 'Thedarwin9876', 'itflab', 3306);
     if (mysqli_connect_errno($conn))
     {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
